16287299ab4c23c4a9aa28594e7e2558c59fef9e
George Toothman <george.toothman@flatironschool.com>
2015-10-12 10:23:51 -0400

    add oo and procedural solutions for 3 and 5 mulitples

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
 # Enter your procedural solution here!
+
+# create an array containing all the numbers between 0..1000
+# iterate through each number checking to see if the remainder is 0
+# if the remainder from the modulo operator is zero, save the num to a new array
+# add all the numbers of the new array together
+
+array = [*1..1000]
+
+new_array = Array.new
+
+array.each do |num|
+  if num % 3 == 0
+    new_array << num
+  elsif num % 5 == 0
+    new_array << num
+  else 
+    nil
+  end
+  new_array
+end
+
+sum = new_array.inject{|sum,x| sum + x }
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
-# Enter your object-oriented solution here!
+class Solution
+  def initialize
+    @array = [*1..1000]
+    @new_array = Array.new
+    @range_matches = self.find_all_matches(array)
+    @sum_matches = self.sum_all_matches(new_array)
+  end
+
+  def find_all_matches(array)
+    array.each do |num|
+      if num % 3 == 0
+        new_array << num
+      elsif num % 5 == 0
+        new_array << num
+      else 
+        nil
+      end
+      new_array
+    end
+  end
+
+  def sum_all_matches(new_array)
+    new_array.inject{|sum,x| sum + x }
+  end
+end
